2aeb1a08e909bfd9fbeae060b8c0da7a6d7f23d5
De Oliveira Tristan <tristan@koji.fr>
2021-02-21 00:27:37 +0100

    fix: go back

--- server/utils/zip.js
+++ server/utils/zip.js
@@ -15,7 +15,7 @@ export function increaseHeading(content: string): string {
 }
 
 async function addToArchive(zip, documents, parentDocument) {
-  let result = "";
+  let result = parentDocument ? `# ${parentDocument.title}\n` : "";
 
   for (const [index, doc] of documents.entries()) {
     const document = await Document.findByPk(doc.id);
